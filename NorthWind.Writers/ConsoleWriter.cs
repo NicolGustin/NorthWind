5368863e1b9d0c444020a9d7ea22457a4f41292f
Nicol Gustin <nicol930519@hotmail.com>
2024-04-14 18:15:47 -0500

    DebugWriter

--- NorthWind.Writers/ConsoleWriter.cs
+++ NorthWind.Writers/ConsoleWriter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NorthWind.Writers
+﻿namespace NorthWind.Writers
 {
     internal class ConsoleWriter : IUserActionWriter
     {
