b3ac1a6c94bed9b3bb53472266d62f4013fd3c2b
Nicol Gustin <nicol930519@hotmail.com>
2024-04-14 18:17:32 -0500

    DependencyContainer

---
+++ NorthWind.Writers/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace NorthWind.Writers
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddConsoleWriter
+            (this IServiceCollection services)
+        {
+            services.AddSingleton<IUserActionWriter, ConsoleWriter>();
+            return services;
+        }
+
+        public static IServiceCollection AddDebugWriter
+            (this IServiceCollection services)
+        {
+            services.AddSingleton<IUserActionWriter, DebugWriter>();
+            return services;
+        }
+
+        public static IServiceCollection AddFileWriter
+            (this IServiceCollection services)
+        {
+            services.AddSingleton<IUserActionWriter, FileWriter>();
+            return services;
+        }
+    }
+}
